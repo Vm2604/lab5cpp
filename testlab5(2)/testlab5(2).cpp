861ebe4335c1c66b66603130f04b7769f22e999d
Mike Penchev <48203051+Vm2604@users.noreply.github.com>
2019-05-20 18:17:17 +0300

    Update testlab5(2).cpp

--- testlab5(2)/testlab5(2).cpp
+++ testlab5(2)/testlab5(2).cpp
@@ -1,37 +1,4 @@
-﻿//#include "pch.h"
-//#include <fstream>
-//#include <stdio.h>
-//#include <conio.h>
-//
-//main()
-//{
-//	FILE *f1, *f2, *h;
-//	char s[1000];
-//	ofstream f1;
-//	f1 = fopen("1.txt", "r");
-//	f2 = fopen("2.txt", "r");
-//	if (f1 == NULL || f2 == NULL)
-//	{
-//		printf("File(s) not found\n");
-//		return 1;
-//	}
-//	h = fopen("buf.txt", "w");
-//	while (fgets(s, 1000, f1) != NULL)
-//		fprintf(h, "%s", s);
-//	f1 = fopen("1.txt", "w");
-//	while (fgets(s, 1000, f2) != NULL)
-//		fprintf(f1, "%s", s);
-//	f2 = fopen("2.txt", "w");
-//	fclose(h);
-//	h = fopen("buf.txt", "r");
-//	while (fgets(s, 1000, h) != NULL)
-//		fprintf(f2, "%s", s);
-//	printf("Done\n");
-//	fclose(f1);
-//	fclose(f2);
-//	fclose(h);
-//	_getch();
-//}
+
 #include"pch.h"
 #include <iostream>
 #include <string>
